bad5f1ec413c10fe0649d5d480bd815950449f35
Bhoomika Singh <bhoomika.elites7676@gmail.com>
2026-03-13 20:23:59 +0530

    Enhance comments and fix expectedSum calculation
    Updated comments to clarify problem, approach, and complexities. Adjusted expectedSum calculation to prevent overflow.

--- Arrays/missingelement.cpp
+++ Arrays/missingelement.cpp
@@ -1,10 +1,13 @@
 //gfg practice questions
-//array based
+// Problem: Missing Number
+// Approach: Use sum formula n(n+1)/2
+// Time Complexity: O(n)- traversing the array 
+// Space Complexity: O(1)- no extra space 
 class Solution{
 public:
 int missingelement(vector<int>& arr){ //created a function for missing element
   int n=arr.size()+1; // find n [size of the array]
-  long expectedSum=(long)n*(n+1)/2;// mathematical formula to calculate the total expected sum
+  long expectedSum=(long)n*(n+1)/2;// mathematical formula to calculate the total expected sum (also using long because to prevent the overflow)
 
   long actualSum=0; //starts from 0 : initialization
   for(int num:arr){// range loop created : element in arr are being stored in num
